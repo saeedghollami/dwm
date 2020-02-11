ba873d12ea75f724fef85badf54275fb6ce95c04
Saeed Gholami <saeed.ghollami@gmail.com>
2020-02-12 03:00:09 +0330

    Make dwm nordic

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "PT Sans:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "PT Sans:size=10:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2e3440"; // bar background 
+static const char col_gray2[]       = "#4c566a"; // !focused win br 
+static const char col_gray3[]       = "#d8dee9"; // bar fg
+static const char col_gray4[]       = "#eceff4"; // br fg selected
+static const char col_cyan[]        = "#81a1c1"; // focused win br ; ch bar
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
